d1db619a0199ff1e5963e21d045b5c590281ddf0
ricroc <rickrocero@gmail.com>
2021-05-14 16:42:01 -0700

    added readme.md, deleted unused exercise apis, and created a new seed file

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -1,27 +1,6 @@
 const db = require('../models')
 const router = require('express').Router();
 
-// Exercise APIs
-// Create an exercise
-router.post('/api/exercises', async (req,res) => {
-    try {
-        const newExercise = await db.Exercise.create(req.body);
-        res.status(200).json(newExercise);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-// Get all exercises
-router.get('/api/exercises', async (req,res) => {
-    try {
-        const allExercises = await db.Exercise.find();
-        res.status(200).json(allExercises);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
 
 // Workout APIs
 // Create a workout 
@@ -61,7 +40,7 @@ router.put('/api/workouts/:id', async (req,res) => {
 // Get total duration of workouts from past 7 days
 router.get('/api/workouts/range', async (req,res) => {
     try {
-        const allWorkouts = await db.Workout.aggregate([{$addFields:{totalDuration:{$sum:"$exercises.duration"}}}]).sort({_id:-1}).limit(7)
+        const allWorkouts = await db.Workout.aggregate([{$addFields:{totalDuration:{$sum:"$exercises.duration"}}}]).sort({_id:-1}).limit(7);
         res.status(200).json(allWorkouts);
     } catch (err) {
         res.status(500).json(err);
